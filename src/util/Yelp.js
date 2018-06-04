e71e9ac08c0b656e6a4e3718413af76046d74fb6
ryanzola <ryanzola@me.com>
2018-06-04 11:55:23 -0400

    search! Hits the yelp api business endpoint, returns array of businesses

---
+++ src/util/Yelp.js
@@ -0,0 +1,36 @@
+const apiKey = 'EqkKKHpRMV7k6jWFOeHDttwT6XLgoh5RbG80KROV4NBhRAtdDxM4Gjrh55H_f-HBQvKLJQt90iMt_qu6iZMu6TPJ80_XkgpuNokASDYtVyGYEEhpRzOvyUT36U8VW3Yx';
+
+let Yelp = {};
+
+Yelp.search = (term, location, sortBy) => {
+  return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+    headers: {
+      Authorization: `Bearer ${apiKey}`
+    }
+  }).then(response => {
+    if(response.ok) {
+      return response.json();
+    }
+    throw new Error('Request failed!');
+  }, networkError => console.log(networkError.message)
+  ).then(jsonResponse => {
+    if(jsonResponse.businesses) {
+      return jsonResponse.businesses.map(business => {
+        return {
+          id: business.id,
+          imgSrc: business.image_url,
+          name: business.name,
+          address: business.location.address1,
+          city: business.location.city,
+          state: business.location.state,
+          zipCode: business.location.zip_code,
+          category: business.categories[0].title,
+          rating: business.rating,
+          reviewCount: business.review_count
+        }
+      })
+    }
+  });
+}
+
+export default Yelp;
